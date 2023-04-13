aa6f86f28562107c2b30b09d386979197050cbfe
Sagar <sagarkhandagre897@gmail..com>
2023-04-13 20:19:47 +0530

    first

---
+++ server.js
@@ -0,0 +1,54 @@
+console.log('Server-side code running');
+
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+
+// serve files from the public directory
+app.use(express.static('public'));
+
+// connect to the db and start the express server
+// connect to the db and start express server
+let db;
+
+const url = "mongodb://localhost:27017";
+
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    return console.log(err);
+  }
+  db = client.db("ClickCounts");
+  // start the express web server listening on 8080
+  app.listen(8080, () => {
+    console.log("listening on 8080");
+  });
+});
+
+// serve the homepage
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// add a document to the DB collection recording the click event
+app.post('/clicked', (req, res) => {
+  const click = {clickTime: new Date()};
+  console.log(click);
+  console.log(db);
+
+  db.collection('clicks').save(click, (err, result) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('click added to db');
+    res.sendStatus(201);
+  });
+});
+
+// get the click data from the database
+app.get('/clicks', (req, res) => {
+  db.collection('clicks').find().toArray((err, result) => {
+    if (err) return console.log(err);
+    res.send(result);
+  });
+});
